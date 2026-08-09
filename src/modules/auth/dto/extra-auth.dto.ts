5fc1061e254a11394b151339e3f8748a176816d2
Parvej Hossain <muhammadparvej98@gmail.com>
2026-08-09 17:29:12 +0600

    feat(auth): added auth related dto

---
+++ src/modules/auth/dto/extra-auth.dto.ts
@@ -0,0 +1,38 @@
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
+export class VerifyEmailDto {
+  @IsString()
+  token: string;
+}
+
+export class ForgotPasswordDto {
+  @IsEmail()
+  email: string;
+}
+
+export class ResetPasswordDto {
+  @IsString()
+  token: string;
+
+  @IsString()
+  @MinLength(8)
+  newPassword: string;
+}
+
+export class LogoutDto {
+  @IsString()
+  refreshToken: string;
+}
+
+export class Enable2faDto {
+  @IsString()
+  code: string;
+}
+
+export class Verify2faDto {
+  @IsString()
+  twoFactorToken: string;
+
+  @IsString()
+  code: string;
+}
